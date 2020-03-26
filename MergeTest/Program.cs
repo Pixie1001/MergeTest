4d48b2094fe81721ab70f3bd3a701198b1ae5f1d
Pixie1001 <45550419+Pixie1001@users.noreply.github.com>
2020-03-26 18:55:48 +1100

    Updating file names

---
+++ MergeTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MergeTest {
+
+    class Program {
+
+        static void Main(string[] args) {
+            Console.WriteLine("Goodbye World!");
+            Console.ReadLine();
+        }
+    }
+}
